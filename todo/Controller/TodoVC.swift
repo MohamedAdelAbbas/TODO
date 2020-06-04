7b802a16188dd63c0f0aaa3d4cf073a8199550ad
Mohamed Adel <m.3del95@gmail.com>
2020-06-04 10:40:32 +0200

    Build Structure of Project

--- todo/Controller/TodoVC.swift
+++ todo/Controller/TodoVC.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TodoVC: UIViewController {
 
+    @IBOutlet weak var todoItemTF: UITextField!
+    @IBOutlet weak var prioritySegment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addTodoPressed(_ sender: UIButton) {
+    }
 }
 
---
+++ todo/View/TodoCell.swift
@@ -0,0 +1,16 @@
+//
+//  TodoCell.swift
+//  todo
+//
+//  Created by Mohamed Adel on 6/4/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import UIKit
+
+class TodoCell: UITableViewCell {
+
+    @IBOutlet weak var itemLabel: UILabel!
+    
+    @IBOutlet weak var priorityView: UIView!
+}
