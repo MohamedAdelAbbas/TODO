130b8c9167cd88a9fd6453881048aaa3e64a8d36
Mohamed Adel <m.3del95@gmail.com>
2020-06-04 11:06:20 +0200

    Add ToDo Model + APIError Model

---
+++ todo/Model/APIError.swift
@@ -0,0 +1,12 @@
+//
+//  APIError.swift
+//  todo
+//
+//  Created by Mohamed Adel on 6/4/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import Foundation
+struct APIError: Codable {
+    let message: String?
+}
---
+++ todo/Model/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  todo
+//
+//  Created by Mohamed Adel on 6/4/20.
+//  Copyright © 2020 Mohamed Adel. All rights reserved.
+//
+
+import Foundation
+struct ToDo: Codable {
+    let item: String?
+    let priority: Int?
+    
+}
